e530e115c80c8d445e282838beaf44d976826416
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2023-08-23 09:51:52 -0300

    funcionando

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { message: 'Olá, Mundo! Sou Marina Delrei, um novo cargo eu ganhei!' });
 });
 
 app.listen(port, () => {
