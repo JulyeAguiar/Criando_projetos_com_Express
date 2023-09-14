c38a843bffdd88e822a9dd035b1218c80e1e6de6
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2023-09-14 15:26:13 -0300

    Site

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {//controller/rota
   res.render('index', { message: 'Olá, Mundo! Sou Marina Delrei, um novo cargo eu ganhei!' });
+});
+app.get('/contatos', (req, res) => {
+  res.render('contato', { message: 'Página de contatos' });
 });
 
 app.listen(port, () => {
